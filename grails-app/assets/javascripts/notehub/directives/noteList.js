9d888c9ac99a24a70cbce0ac143132ce9ddc0d65
Paul Li <paul.j.li.97@gmail.com>
2017-03-02 17:14:41 -0500

    Added Query String Parser

--- grails-app/assets/javascripts/notehub/directives/noteList.js
+++ grails-app/assets/javascripts/notehub/directives/noteList.js
@@ -31,8 +31,19 @@ function noteList() {
     return directive;
 
     /*@ngInject*/
-    function NoteListController($http,$scope) {
+    function NoteListController($http,$scope,$location) {
+
         var vm = this;
+
+        var goto = $location.absUrl();
+        var parser = document.createElement('a');
+        parser.href = goto;
+        var queryString = parser.search;
+        var idNum = getQueryVariable('id'); // gets query variable
+
+        console.log(idNum);
+        vm.getId = goto;
+
         vm.orderProp = 'dateCreated';
 
         // used for ng-repeat num number of times.
@@ -49,16 +60,19 @@ function noteList() {
         //Dummy Data
         vm.posts =[
             {
+                id: 2,
                 title: "Java Tutorial",
                 author: "Paul Li",
                 dateCreated: "11/11/16",
                 rating: 5
             },{
+                id: 3,
                 title: "Calculus 101",
                 author: "Bob Ross",
                 dateCreated: "10/9/16",
                 rating: 4
             },{
+                id: 4,
                 title: "French for Dummies",
                 author: "Will Smith",
                 dateCreated: "10/8/16",
@@ -66,4 +80,15 @@ function noteList() {
             }
         ]
     }
+}
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
 }

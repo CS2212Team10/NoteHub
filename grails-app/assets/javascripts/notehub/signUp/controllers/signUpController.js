f5d1b4962eefd93808d7ed56376684419d194b47
Paul Li <paul.j.li.97@gmail.com>
2017-03-20 21:09:22 -0400

    Removed some inline styling + added Reset Button +mismatched passwords.

--- grails-app/assets/javascripts/notehub/signUp/controllers/signUpController.js
+++ grails-app/assets/javascripts/notehub/signUp/controllers/signUpController.js
@@ -4,21 +4,121 @@ angular
     .module("notehub.signUp")
     .controller("SignUpController", signUpController);
 
-function signUpController(applicationDataFactory, contextPath, $state,$scope) {
+function signUpController(applicationDataFactory, contextPath, $state,$scope,$http) {
 
     $scope.title = "hello";
 
-    /* IDK WHAT THIS DOES
-     var vm = this;
+    var emptyUserInfo = {
+        username: "",
+        fullname: "",
+        email: "",
+        password: "",
+        retypepassword: ""
+    };
 
-     vm.contextPath = contextPath;
+    $scope.error = {
+        passwordMatch: false,
+        properEmailFormat: false,
+        duplicateEmail: false,
+        duplicateName: false
+    };
+    console.log($scope.error);
+    /*
+    var newUser = {
+        name:"",
+        email:"",
+        password:"",
+        picture:""
+    }; */
 
-     applicationDataFactory.get().then(function(response) {
-     vm.applicationData = response.data;
-     });
+    $scope.emailFormat = /^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/;
 
-     vm.stateExists = function(name) {
-     return $state.get(name) != null;
-     };
-     */
+    $scope.originForm = angular.copy($scope.emptyUserInfo); //for resetting the form.
+    $scope.resetError = angular.copy($scope.error); //for resetting the form.
+
+
+    $scope.signUpClick = function(user){
+        var data ={
+            name: user.username,
+            email: user.fullname,
+            password: user.password,
+            picture:"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAPAAAADwCAYAAAA+VemSAAAgAEl...=="
+        };
+        console.log("loaded user data");
+        //validate the password
+        if($scope.checkMatchPass(data.password, user.retypepassword) == false){
+            return false;
+        }
+
+        $http.post('/user/', JSON.stringify(data)).then(function (response) {
+            console.log(response.data);
+            console.log("loaded");
+            if (response.data) {
+                $scope.msg = "Put Data Method Executed Successfully!";
+                $window.location.href = "/home?user=" + response.data;
+            }
+            console.log($scope.msg);
+        }, function (response) {
+            $scope.msg = "Service not Exists";
+            $scope.resetForm();
+            console.log(response);
+            console.log($scope.msg);
+            $scope.statusval = response.status;
+            $scope.statustext = response.statusText;
+            $scope.headers = response.headers();
+        });
+    };
+
+    $scope.checkMatchPass = function(password1, password2){
+        if (password1 != password2){
+            console.log("passwords don't match");
+            $scope.error.passwordMatch = true;
+            $scope.resetForm();
+            return false; //TODO fix what ever this is supposta do (spit our error)
+
+        }else{
+            console.log("passwords matched");
+            return true;
+        }
+
+    };
+
+    //TODO fix reset
+    $scope.resetForm = function(){
+        console.log("wtf");
+        $scope.newUserInfo = angular.copy($scope.originForm); // Assign clear state to modified form
+        $scope.signUpForm.$setPristine(); // this line will update status of your form, but will not clean your data, where `registrForm` - name of form.
+    };
+    $scope.resetErrors = function() {
+        $scope.error = angular.copy($scope.resetError);
+
+    }; // Assign clear state to modified form
+    $scope.raiseError = function(error){
+        switch (error){
+            case 0:
+                $scope.notification = "Please fill in all the boxes";
+                break;
+            case 1:
+                $scope.notification = "This email has been used already";
+                break;
+            case 2:
+                $scope.notification = "This username has been used already";
+                break;
+            case 3:
+                $scope.notification = "The passwords don't match";
+                break;
+            case 4:
+                $scope.notification = "Invalid email"
+                break;
+        }
+    };
+
+    $scope.equalPassword = function (password1, password2) {
+        return (password1 == password2);
+    };
+
+    $scope.validEmail = function(email){
+        //if email is good return true
+        //elase false
+    };
 }
--- grails-app/views/signUp.gsp
+++ grails-app/views/signUp.gsp
@@ -21,42 +21,39 @@ input,label { font-size:10pt; font-family:Arial, Helvetica, sans-serif; }
 
     <h3 style="margin:0 auto 20px auto; width:175px; font-size: 40px">Sign Up</h3>
 
-    <p style="margin:0 auto; width:275px; text-align:left;">User Name</p>
+    <div role="alert">
+        <div ng-show="submitted">
+        <span class="alert alert-warning" ng-show="error.passwordMatch">
+            Passwords don't match!</span>
+        <span class="alert alert-warning" ng-show="error.properEmailFormat">
+            Not valid email!</span>
+        </div>
+    </div>
+    <br>
 
-    <form method="post" enctype="multipart/form-data">
-        <input style="width:275px; margin:0 auto; padding:0;" type="text" name="Note Name" value="" placeholder="User Name" ng-model="username"><br>
-    </form>
+    <form name="signUpForm" ng-submit="signUpClick(newUserInfo)">
 
-    <p style="margin:0 auto; width:275px; text-align:left;">Full Name</p>
+        <p style="margin:0 auto; width:275px; text-align:left;">User Name</p>
+        <input type="text" name="username" value="" placeholder="User Name" ng-model="newUserInfo.username"><br>
 
-    <form method="post" enctype="multipart/form-data">
-        <input style="width:275px; margin:0 auto; padding:0;" type="text" name="Note Name" value="" placeholder="Full Name" ng-model="fullname"><br>
-    </form>
+        <p style="margin:0 auto; width:275px; text-align:left;">Full Name</p>
+        <input type="text" name="fullname" value="" placeholder="Full Name" ng-model="newUserInfo.fullname"><br>
 
-    <p style="margin:0 auto; width:275px; text-align:left;">uwo email</p>
+        <p style="margin:0 auto; width:275px; text-align:left;">Email</p>
+        <input type="text" name="email" value="" placeholder="Email Address" ng-model="newUserInfo.email"><br>
 
-    <form method="post" enctype="multipart/form-data">
-        <input style="width:275px; margin:0 auto; padding:0;" type="text" name="Note Name" value="" placeholder="uwo email" ng-model="uwoemail"><br>
-    </form>
+        <p style="margin:0 auto; width:275px; text-align:left;">Password</p>
+        <input type="text" name="password" value="" placeholder="Password" ng-model="newUserInfo.password"><br>
 
-    <p style="margin:0 auto; width:275px; text-align:left;">Password</p>
-
-    <form method="post" enctype="multipart/form-data">
-        <input style="width:275px; margin:0 auto; padding:0;" type="text" name="Note Name" value="" placeholder="Password" ng-model="password"><br>
-    </form>
-
-    <p style="margin:0 auto; width:275px; text-align:left;">Re-Type Password</p>
-
-    <form method="post" enctype="multipart/form-data">
-        <input style="width:275px; margin:0 auto; padding:0;" type="text" name="Note Name" value="" placeholder="Re-Type Password" ng-model="retypepassword"><br>
-    </form>
+        <p style="margin:0 auto; width:275px; text-align:left;">Re-Type Password</p>
+        <input type="text" name="retypepassword" value="" placeholder="Re-Type Password" ng-model="newUserInfo.retypepassword"><br>
 
     <br>
 
     <p style="margin:0 auto; width:275px; text-align:left;">Upload a Profile Picture</p>
     <div style="border:1px solid #000000; width:275px; padding:5px 0;
     margin:0px auto;text-align:center;">
-        <textarea rows="4" cols="50" ng-model="content">
+        <textarea rows="4" cols="50" ng-model="picture">
             <p style="margin:0 auto; width:275px; text-align:left; padding-top:10px;"></p>
         </textarea>
         <label for="file-upload" style="border:0; line-height:30px; display:inline-block; width:255px; height:30px; background-color:#3399FF; color:#FFF;">Browse</label>
@@ -64,10 +61,10 @@ input,label { font-size:10pt; font-family:Arial, Helvetica, sans-serif; }
 
     </div>
     <br>
-        <a style="border:0; width:275px; height:30px; background-color:#3399FF; color:#FFF;" href="home" class="btn">
-                Sign Up
-        </a>​
+        <button class="btn" type="submit" value="Upload" ng-click="submitted = true" ng-disabled="">Sign In</button>
 
+    </form>
+    ​<button class="btn" ng-click="resetForm()">Reset</button>​
 </div>
 </body>
 </html>
